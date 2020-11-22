f79147a7c3b37309f02f2cb71c566d09acaf4280
Vinicius Pimenta <62510504+vinicius-pimenta@users.noreply.github.com>
2020-11-22 18:44:01 -0300

    Create A.java

---
+++ A.java
@@ -0,0 +1,29 @@
+public class A{
+  // atributos
+  int A1;
+  float A2;
+
+  public int getA1(){
+    return A1;
+  }
+
+  public void setA1(int A1){
+    this.A1 = A1;
+  }
+
+  public float getA2(){
+    return A2;
+  }
+
+  public void setA2(float A2){
+    this.A2 = A2;
+  }
+
+  public void MA1(){
+    System.out.println("MA1");
+  }
+
+  public void MA2(){
+    System.out.println("MA2");
+  }
+}
